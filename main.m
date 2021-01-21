e51e052ae0ec407faaafac28f8365ff43ca4618f
niklauskun <65974644+niklauskun@users.noreply.github.com>
2021-01-20 22:17:01 -0500

    Update transition matrix for RTP

---
+++ main.m
@@ -0,0 +1,77 @@
+load('RTP_NYC_2010_2019.mat')
+Ts = 1/12; % time step
+DD = 365; % select days to look back
+lambda = reshape(RTP(:,(end-DD):end),numel(RTP(:,(end-DD):end)),1); 
+T = numel(lambda); % number of time steps
+
+%%
+Pr = .5; % normalized power rating wrt energy rating
+P = Pr*Ts; % actual power rating taking time step size into account
+eta = .9; % efficiency
+c = 10; % marginal discharge cost - degradation
+ed = .001; % SoC sample granularity
+ef = .0; % final SoC target level, use 0 if none
+Ne = floor(1/ed)+1; % number of SOC samples
+e0 = .5;
+
+vEnd = zeros(Ne,1);  % generate value function samples
+
+vEnd(1:floor(ef*100)) = 1e2; % use 100 as the penalty for final discharge level
+
+
+%%
+tic
+v = zeros(Ne, T+1); % initialize the value function series
+% v(1,1) is the marginal value of 0% SoC at the beginning of day 1
+% V(Ne, T) is the maringal value of 100% SoC at the beginning of the last operating day
+v(:,end) = vEnd; % update final value function
+
+% process index
+es = (0:ed:1)';
+Ne = numel(es);
+% calculate soc after charge vC = (v_t(e+P*eta))
+eC = es + P*eta; 
+% round to the nearest sample 
+iC = ceil(eC/ed)+1;
+iC(iC > (Ne+1)) = Ne + 2;
+iC(iC < 2) = 1;
+% calculate soc after discharge vC = (v_t(e-P/eta))
+eD = es - P/eta; 
+% round to the nearest sample 
+iD = floor(eD/ed)+1;
+iD(iD > (Ne+1)) = Ne + 2;
+iD(iD < 2) = 1;
+
+for t = T:-1:1 % start from the last day and move backwards
+    vi = v(:,t+1); % input value function from tomorrow
+    vo = CalcValueNoUnc(lambda(t), c, P, eta, vi, ed, iC, iD);
+    v(:,t) = vo; % record the result 
+end
+
+tElasped = toc;
+
+%% convert value function to 5 segments
+vAvg = zeros(5,T+1);
+
+NN = (Ne-1)/5;
+
+for i = 1:5
+   vAvg(i,:) = mean(v((i-1)*NN + (1:(NN+1)),:)); 
+end
+
+
+%% perform the actual arbitrage
+eS = zeros(T,1); % generate the SoC series
+pS = eS; % generate the power series
+
+e = e0; % initial SoC
+
+for t = 1:T % start from the first day and move forwards
+    vv = v(:,t+1); % read the SoC value for this day
+   [e, p] =  Arb_Value(lambda(t), vv, e, P, 1, eta, c, size(v,1));
+   eS(t) = e; % record SoC
+   pS(t) = p; % record Power
+end
+
+ProfitOut = sum(pS.*lambda) - sum(c*pS(pS>0));
+solTimeOut = toc;
---
+++ test_pre_processing.py
@@ -0,0 +1,63 @@
+import os
+import pandas as pd
+import numpy as np
+from scipy.io import loadmat
+import timeit
+
+initial = timeit.default_timer()
+
+def transition_matrix(df, state_num):
+    n = state_num
+    M = [[0]*n for _ in range(n)]
+    for x in range(df.shape[1]):
+        transitions = df[x]
+        for (i,j) in zip(transitions,transitions[1:]):
+            M[i][j] += 1
+
+    #now convert to probabilities:
+    for row in M:
+        s = sum(row)
+        if s > 0:
+            row[:] = [f/s for f in row]
+    return M
+
+
+#RTP data
+BASE_DIRECTORY = 'C:\\Users\\wenmi\\Desktop\\MarkovESValuation'
+RESULTS_DIRECTORY = os.path.join(BASE_DIRECTORY, 'transition_matrix')
+mat = loadmat(os.path.join(BASE_DIRECTORY, 'RTP_NYC_2010_2019.mat'))
+RTP = pd.DataFrame(mat['RTP'])
+resolution = 12 #slices in one hour
+price_bar = 200 #price greater than price bar is price spike
+state_gap = 10 #state gap
+state_num = price_bar//state_gap + 2 #state number]
+transition_matrix_num = 24
+
+for i in range(transition_matrix_num):
+    start = i*resolution
+    end = (i+1)*resolution
+    hour_data = (RTP[start : end]//state_gap+1).astype(int)
+    hour_data[hour_data < 0] = 0
+    hour_data[hour_data > state_num-2] = state_num-1
+    m = transition_matrix(hour_data, state_num)
+    np.savetxt(os.path.join(RESULTS_DIRECTORY,'hour'+str(i)+".csv"), m, delimiter = ',')
+
+stop = timeit.default_timer()
+
+print('Time: ', stop - initial)
+
+#import datetime
+
+#initial_date = datetime.datetime.strptime('2010-01-01 00:00:00', '%Y-%m-%d %H:%M:%S')
+#time_resolution = 300 #data resolution in sec, defalut by 5 mins#
+#states_resolution = 1000
+
+#for i,j in [(i,j) for i in range(df.shape[0]) for j in range(df.shape[1])]:
+#    delta = datetime.timedelta(days = j,seconds = i * time_resolution)
+#    t = initial_date + delta
+#    d = df.iloc[i,j]
+#    l = d//states_resolution
+#    demand_rt = demand_rt.append({'time': t, 'demand(MW)': d, 'demand_level': l}, ignore_index=True)
+#demand_rt.sort_values('time', inplace=True)
+#demand_rt.to_csv(os.path.join(BASE_DIRECTORY,"real_time_demand.csv"), index=False)
+
